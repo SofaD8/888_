df82e5b4bc9d801c4e1779eadd4d5ca2272bd009
SofaD8 <161923459+SofaD8@users.noreply.github.com>
2024-04-26 19:27:57 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,19 +1,22 @@
+#Напишіть функцію, яка приймає рядкові дані та виконує перевірку на їхню відповідність мейлу.
+#Вимоги:
+#-Цифри (0-9).
+#-лише латинські літери у великому (A-Z) та малому (a-z) регістрах.
+#-у тілі мейла допустимі лише символи "_" і "-". Але вони не можуть бути першим символом мейлу.
+#-Символ "-" не може повторюватися.
 import re
 
 
-# Функція для валідації електронної адреси
-def validate_email(email):
-    # Регулярний вираз для перевірки електронної адреси
-    pattern = r'^(?![_-])[A-Za-z0-9]+(?:[._-]?[A-Za-z0-9]+)*@[A-Za-z0-9-]+(?:\\.[A-Z|a-z]{2,})+$'
+def is_valid_email(email):
+    regex = r"^(?![_-])[A-Za-z0-9._-]+(?<![-])@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
 
-    # Перевірка електронної адреси за допомогою регулярного виразу
-    if re.match(pattern, email):
-        return True
-    else:
-        return False
+    if re.match(regex, email):
+        if "--" not in email.split('@')[0]:
+            return True
+    return False
 
 
-# Приклад використання
-email = "example_email@example.com"
-is_valid = validate_email(email)
-print(f"Електронна адреса {email} є {'валідною' if is_valid else 'невалідною'}.")
+test_emails = ["example_email@example.com", "_example@example.com", "example--email@example.com",
+               "example-email@example.com", "example-email@example.io.com"]
+for email in test_emails:
+    print(f"Is '{email}' a valid email? {is_valid_email(email)}")
