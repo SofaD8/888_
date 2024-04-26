e3cca294839f0e46fa2d4d15bba8160043c59dd3
SofaD8 <161923459+SofaD8@users.noreply.github.com>
2024-04-26 19:27:09 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,19 +1,16 @@
+#Напишіть функцію, яка виконує валідацію номера банківської картки (9999-9999-9999-9999).
 import re
 
 
-# Define a function to validate a credit card number
 def validate_credit_card(card_number):
-    # Define the regular expression pattern for a credit card number
-    pattern = r'^\\d{4}-\\d{4}-\\d{4}-\\d{4}$'
+    pattern = r'^[0-9]{4}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$'
 
-    # Use the regular expression to match the card number
     if re.match(pattern, card_number):
         return True
     else:
         return False
 
 
-# Example usage
 card_number = "9999-9999-9999-9999"
 is_valid = validate_credit_card(card_number)
 print(f"The card number {card_number} is {'valid' if is_valid else 'invalid'}.")
