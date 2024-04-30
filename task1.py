5b60874980e1899b22cc8d5492f652abd668d35d
SofaD8 <161923459+SofaD8@users.noreply.github.com>
2024-04-30 12:55:41 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,15 @@
 #Напишіть регулярний вираз, який знаходитиме в тексті фрагменти, що складаються з однієї літери R,
 # за якою слідує одна або більше літер b, за якою одна r. Враховувати верхній та нижній регістр.
-r`[Rr][Bb]+[Rr]`
+import re
+
+
+def check_pattern(text):
+    pattern = r`[Rr][Bb]+[Rr]`
+    return re.findall(pattern, text)
+
+
+text_to_check = "rRbr R bbr RBBBBBr Rbbr"
+matches = check_pattern(text_to_check)
+print(matches)
+
+
